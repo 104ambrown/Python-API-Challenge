936a4d1463974dbeb6cd319197d87db3daeeda8e
Zas Mama <104ambrown@gmail.com>
2021-10-23 15:33:41 -0400

    added final touches and output

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ee387d6be84b0a06aeb6816797f3caa2"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDGz1cL9R6G0qi8JZ2ddtR_rWsL8QcmHvQ"
